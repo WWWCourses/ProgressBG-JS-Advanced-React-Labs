d1e5d3ec7c59f9943c07740e1ee0545517bd4bc1
WWWCourses <ivawebcourses@gmail.com>
2019-07-14 20:23:38 +0300

    simplify

--- topics-examples/src/components/ListsAndKeys.js
+++ topics-examples/src/components/ListsAndKeys.js
@@ -1,26 +1,18 @@
 import React from 'react';
 
-function ItemsList(props) {
-    const items = ['Item1', 'Item2', 'Item3'];
-    return (
-        <ul>
-            {items.map( (item, index) => <Item key={index} item={item} />)}
-        </ul>
-    )
-}
-
 function Item(props) {
     return (
-        <li>{props.item}</li>
+        <li>{props.skill}</li>
     )
 }
 
 export function ListsAndKeys(props) {
     const skills = ['HTML', 'CSS', 'JS'];
 
-    return  (
-       <div><ItemsList/></div>
-
+    return (
+        <ul>
+            {skills.map( (skill, index) => <Item key={index} skill={skill} />)}
+        </ul>
     )
 }
 
