6aab5d398eb0d333718d6e103e1b7793fcd3256c
WWWCourses <ivawebcourses@gmail.com>
2020-12-15 18:33:51 +0200

    Lab18: 10.12.2020

---
+++ lab18/removeObjectFromArray.js
@@ -0,0 +1,14 @@
+let array = [
+	{id: 1},
+	{id: 4},
+	{id: 5},
+]
+
+let id = 4;
+
+let idx = array.findIndex( (obj)=> obj.id === id );
+console.log(idx);
+
+
+// remove object with id === 2
+
---
+++ lab19/promises/test.js
@@ -0,0 +1,20 @@
+let cleanRoom = new Promise(function(resolve, reject) {
+	setTimeout(function() {
+		resolve(`cleanRoom: resolved after 1s`);
+	}, 1000);
+});
+
+
+let eatIcecream = function(){
+	return new Promise(function (resolve, reject){
+		console.log(`Icecream time! `);
+		setTimeout( _=>{
+			resolve('eatIcecream: resolved after 2s')
+		},2000)
+	})
+}
+
+cleanRoom
+.then()
+.then(p=>console.log(p))
+
