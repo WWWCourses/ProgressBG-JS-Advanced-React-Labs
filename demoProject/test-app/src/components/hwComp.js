504474615499ced5c9c4be112a06428d0b135bd6
WWWCourses <ivawebcourses@gmail.com>
2021-01-12 14:08:47 +0200

    done:HW21

--- demoProject/test-app/src/components/hwComp.js
+++ demoProject/test-app/src/components/hwComp.js
@@ -2,7 +2,18 @@ import "./JSXdemo.css";
 
 let numbers = [1,2,3,4];
 
-// YOUR CODE HERE
+const HWComp = ()=>{
+	return (
+		<div>
+			<h1>HW: lists with dynamic borders</h1>
+			{numbers.map( (el,i)=>
+				<li key={i} className={el%2?"blue-border":"red-border"}>
+					Item {el}
+				</li>
+			)}
+		</div>
+	)
+}
 
 
 export default HWComp
