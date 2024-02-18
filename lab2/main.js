99cf58224359547fe6ebac43d47f96e5c7d9a7c8
WWWCourses <ivawebcourses@gmail.com>
2024-02-18 19:13:50 +0200

    Lab2: 18.02.2024 DONE: JavaScript Overview

---
+++ lab2/main.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector('button')
+const h1 = document.querySelector('h1')
+
+btn.addEventListener('click', function(e) {
+    h1.style.backgroundColor = '#F00'
+})
---
+++ lab2/tmp.js
@@ -0,0 +1,51 @@
+// console.log(1)
+// console.log(2)
+// console.log(x);
+// console.log(3)
+
+
+// let student = {
+//     'name': 'Ada',
+//     'age':23
+// }
+
+// console.log( student[0] );
+// console.log( student[age] ); //?
+
+
+
+
+
+
+// let x = 4
+// console.log( typeof(x) );
+
+// x = 'a'
+// console.log(x);
+
+// class Student{
+
+// }
+
+// let pesho = new Student()
+// console.log(pesho);
+
+// let arr = [1, 'a', [2,3] ]
+
+
+// console.log( arr[1]);
+
+
+const x = 'a'
+
+console.log( x.toUpperCase() );
+console.log( (new String('a') ).toUpperCase() );
+
+// Primitive Boxing (Wrapping)
+
+const arr = [1]
+arr.id = 1
+console.log(arr.id);
+
+x.id = 1
+console.log( x.id );
